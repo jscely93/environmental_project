cf529e8830243a127f10ce25d30fef1c7a111abf
jscely93 <53489061+jscely93@users.noreply.github.com>
2023-02-01 23:42:20 -0500

    Change script

--- script_analisis_prueba_tecnica.R
+++ script_analisis_prueba_tecnica.R
@@ -272,6 +272,11 @@ ci
 exp(coef(m))
 exp(cbind(OR = coef(m), ci))
 
+# Probar que se cumple el supuesto del paralelismo
+brant(m)
+
+#Se cumple el supuesto
+
 # Tabla APA regresión ordinal
 tbl_logreg <- tbl_regression(modelo_1, exponentiate = TRUE,
                              label = c(years_resid ~ "Años de residencia", env_home ~ "Sector")) %>%
